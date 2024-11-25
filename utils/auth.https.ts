f369047db5b6d05a93e051fb98414f0ce38d660f
Maria <mariagracielaesquivelfernandez@gmail.com>
2024-11-25 13:25:58 -0300

    feat(auth): add login and register HTTP methods

---
+++ utils/auth.https.ts
@@ -0,0 +1,58 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL + "/auth";
+
+export const loginHttp = async ({
+  user,
+}: {
+  user: {
+    email: string;
+    password: string;
+  };
+}) => {
+  try {
+    const response = await fetch(`${API_URL}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+
+    if (!response.ok) {
+      throw new Error("Error al iniciar sesión");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error: any) {
+    throw new Error(error.message || "Error al iniciar sesión");
+  }
+};
+
+export const register = async ({
+  user,
+}: {
+  user: {
+    fullName: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+  };
+}) => {
+  try {
+    const response = await fetch(`${API_URL}/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+
+    if (!response.ok) {
+      throw new Error("Error al registrar");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error: any) {
+    throw new Error(error.message || "Error al registrar");
+  }
+};
